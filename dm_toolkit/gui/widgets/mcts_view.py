39a80166a71297ef61a14da182c574c61887fa6e
16KB <e1423047@oit.ac.jp>
2025-12-09 20:30:05 +0900

    調整

--- dm_toolkit/gui/widgets/mcts_view.py
+++ dm_toolkit/gui/widgets/mcts_view.py
@@ -1,4 +1,3 @@
-# -*- coding: cp932 -*-
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QLabel, QProgressBar, QHeaderView, QTabWidget
 from PyQt6.QtCore import Qt
 from .mcts_graph_view import MCTSGraphView
